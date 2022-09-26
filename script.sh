ccefe22906987f9da7cf0d5bd2db031a2b9be9a7
gn1m1ew <46466611+gn1m1ew@users.noreply.github.com>
2022-09-25 21:09:49 -0700

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Define the filename
+filename1='firstFile.txt'
+filename2='secondFile.txt'
+
+git init
+# Type the text that you want to append
+read -p "Enter the text that you want to append:" newtext
+
+# Check the new text is empty or not
+if [ "$newtext" != "" ]; then
+      # Append the text by using '>>' symbol
+      sudo echo $newtext >> $filename1
+fi
+git add firstFile.txt
+git commit -m "first commit"
+git checkout -b "mingwei/hw1"
+git checkout "mingwei/hw1"
+# Type the text that you want to append
+read -p "Enter the text that you want to append:" newtext2
+
+# Check the new text is empty or not
+if [ "$newtext" != "" ]; then
+      # Append the text by using '>>' symbol
+      sudo echo $newtext2 >> $filename1
+fi
+git add firstFile.txt
+git commit -m "branch hw1 commit"
+git checkout master
+# Type the text that you want to append
+read -p "Enter the text that you want to append:" newtext3
+
+# Check the new text is empty or not
+if [ "$newtext" != "" ]; then
+      # Append the text by using '>>' symbol
+      sudo echo $newtext3 >> $filename2
+fi
+git add secondFile.txt
+git commit -m "branch main new work commit"
+git merge "mingwei/hw1" -m "merge"
+git log --graph
